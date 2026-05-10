475052174e50491c24f19ca3a4f62778026c0400
Sonix <SonixXD@protonmail.com>
2026-05-10 10:31:25 +0000

    Project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+#include <zlib.h>
+#include <openssl/sha.h>
+
+int main() {
+    
+    std::cout << "nanogit alive.\n";
+
+    return 0;
+}
